1144008a82c908e2dffe6cb5f016f9fa1971d09e
Vector <ronaldvector210@gmail.com>
2025-11-27 14:13:22 -0500

    Solucionado: Subiendo el contenido completo de las carpetas como archivos normales.

---
+++ SYS_VENTAS/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * El sistema de venta como proyecto final debe incluir:
+ * -> BIBLIOTECAS
+ * -> CLASES
+ * -> METODOS
+ * -> ESTRUCTURAS REPETITIVAS
+ * -> ESTRUCTURAS CONDICIONALES
+ * -> ARREGLOS UNIDIMENSIONALES
+ * -> ARREGLOS BIDIMENSIONALES
+ * -> VARIABLES
+ */
+using System;
+using Biblio_SysVentas;//Llamamos a nuestra biblioteca
+
+namespace SYS_VENTAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            do
+            {
+                opcion = ClaseMenu.MenuPrincipaDinamico();
+
+                Console.ReadKey();
+                Console.Clear();
+
+            } while (opcion != 5);
+        }
+    }
+}
